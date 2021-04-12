45c8af38ae6f2faee7fe5e3d27466ff1520daed2
Alina Khomenker <alinahom@me.com>
2021-04-12 21:27:42 +0300

    add router route

--- back/routes/route.js
+++ back/routes/route.js
@@ -1,7 +1,23 @@
 
 const express = require('express');
 const router = express.Router();
-const Data = require('../model/schema')
+const Data = require('../models/model')
 
+router.route('/post').post((req,res) => {
+    const newData = {
+        title: req.body.title,
+        description: req.body.description
+    };
+
+    const newDataPost = new Data(newData);
+
+    newDataPost.save((error) => {
+        if(error){
+            res.status(500).json({msg:'Sorry,internal server errors'});
+        }else{
+            res.status(200).json({msg:'Data has been saved'});
+        }
+    });
+});
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,58 +1,63 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const postSchema = require('./model/schema');
+//const postSchema = require('./models/model');
 
 
-const app = express();
 //data parsing
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cors());
+
+/*app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+);*/
+
 
 //connect to db bu url
-mongoose.connect('mongodb+srv://alin:Ft1gLRF8H8wh@cluster0.unayf.mongodb.net/list-db?retryWrites=true&w=majority', {
+//'mongodb+srv://alin:Ft1gLRF8H8wh@cluster0.unayf.mongodb.net/list-db?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://alin:Ft1gLRF8H8wh@cluster0.unayf.mongodb.net/list-db', {
     useNewUrlParser:true,
     useUnifiedTopology:true});
 
 //router
 //app.use('/',require('./routes/route'))
 
-app.get('/',function (req,res) {
+/*app.get('/',function (req,res) {
     postSchema.find({},function (list) {
 
     });
     res.send('express here!');
-})
+})*/
 
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        credentials: true,
-    })
-);
+app.use('/',require('./routes/route'));
 
 
-app.post('/post', function(request, response) {
+/*app.post('/post', function(request, response) {
 
-    const newData = {
-        title: request.body.title,
-        description: request.body.description,
-    };
+  const newData = {
+       title: request.body.title,
+       description: request.body.description
+   };
 
-    const newDataPost = new postSchema(newData);
+   const newDataPost = new postSchema(newData);
 
-    newDataPost.save((error) => {
-        if(error){
-            response.status(500).json({msg:'Sorry,internal server errors'});
-        }else{
-            response.status(200).json({msg:'Data has been saved'});
-        }
-    });
+   newDataPost.save((error) => {
+       if(error){
+           response.status(500).json({msg:'Sorry,internal server errors'});
+       }else{
+           response.status(200).json({msg:'Data has been saved'});
+       }
+   });
 
-    //console.log(newDataPost);
+   //console.log(newDataPost);
 });
-
+*/
 
 app.listen(3001,function () {
     console.log("server started");
